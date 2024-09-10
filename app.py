c67c8b666d2c60d32703725c518e47159159c016
Samrat Singh <samrat.singhnepal@gmail.com>
2024-09-10 17:01:07 +0545

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import time
 import os
